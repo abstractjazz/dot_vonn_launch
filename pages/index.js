d45edda4a93043139a961d591f331b9788ec84af
abstractjazz <evan@heyevan.com>
2022-10-01 22:23:12 -0700

    layout fix

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
 import MainPage from '../components/MainPage'
-import Portfolio from '../components/Portfolio'
 import HowItWorks from '../components/HowItWorks'
 import Contact from '../components/Contact'
 import FAQ from '../components/FAQ'
 import CTA from '../components/CTA'
 import Services from '../components/Services'
-import MainPage2 from '../components/MainPage2'
+
 
 export default function Main() {
   return (
