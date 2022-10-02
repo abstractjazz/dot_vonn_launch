bdef5d78e2a506b623ceacfd9784ac5d4c1c18ff
abstractjazz <evan@heyevan.com>
2022-10-02 00:25:00 -0700

    adds success page

---
+++ pages/success.js
@@ -0,0 +1,6 @@
+export default function Success() {
+    return <div className=" w-full h-screen flex flex-col items-center justify-center bg-[url('/success_smileys.png')] bg-cover">
+        <h1 className=" bg-white rounded-md text-5xl font-bold shadow-md px-12 py-4 mb-24">Thanks for reaching out! We&apos;ll be in touch soon.</h1>
+        <a className="text-3xl bg-[#ff66ea]/90 rounded-md font-bold" href={'/'}>Home</a>
+        </div>
+  }
