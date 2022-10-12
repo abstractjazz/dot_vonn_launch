f0cfbe326b2da2d05f3d0a34d20b4e4dff59bab4
abstractjazz <evan@heyevan.com>
2022-10-12 02:13:30 -0700

    toml env tweak

--- next.config.js
+++ next.config.js
@@ -2,10 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  publicRuntimeConfig: {
-    NEXT_PUBLIC_GOOGLE_ANALYTICS: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
 }
-  }
+  
 
 
 module.exports = nextConfig
