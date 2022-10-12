eca6191a0ad377bb619ea1e86c59e00b94416658
abstractjazz <evan@heyevan.com>
2022-10-11 21:09:45 -0700

    adds GA

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,17 @@ function MyApp({ Component, pageProps }) {
 return (
   <>
   <Script strategy="lazyOnload" src={`<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+  <Script strategy="lazyOnload">
+    {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+    page_path: window.location.pathname,
+     });
+    `}
+  
+</Script>
   <Head>
     <title>Dot Vonn</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
