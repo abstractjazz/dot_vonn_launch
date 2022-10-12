77bef25a130d830af4e3463eae4e9720e3a5a585
abstractjazz <evan@heyevan.com>
2022-10-11 23:18:47 -0700

    removes quotes from ga key

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,18 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
 return (
   <>
-  <Script id="loads-GA" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
-  <Script id="GA-param" async >
+  <Script id="loads-GA" strategy='lazyOnload' src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+  <Script id="GA-param" strategy='lazyOnload' >
     {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {page_path: window.location.pathname,});
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname,
+      });
     `}
 </Script>
+
+
   <Head>
     <title>Dot Vonn</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
