69e2fb987c14fd9bf3b9bd4e1404329b6b7a7c5a
abstractjazz <evan@heyevan.com>
2022-10-11 22:01:32 -0700

    adds page path

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ return (
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',);
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {page_path: window.location.pathname,});
     `}
 </Script>
   <Head>
