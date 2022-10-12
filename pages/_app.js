9d1b76df93e9d25703f438082c6921002883d4d8
abstractjazz <evan@heyevan.com>
2022-10-11 20:32:30 -0700

    adds head

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,15 @@ return (
     <title>Dot Vonn</title>
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <link rel='dot vonn icon' href="/tab-favicon.png"/>
+    <meta name="description" content="The best websites for creative entrepreneurs"/>
+        <meta name="author" content="Dot Vonn Web And Content Studio"/>
+       
+        <meta name="robots" content="index, follow"/>
+        <meta property="og:title" content="Dot Vonn Web and Content Studio."/>
+        <meta property="og:site_name" content="Dot Vonn"/>
+        <meta property="og:description" content="Top-tier creative and technical services, accessibly priced."/>
+        <meta property="twitter:title" content="Dot Vonn Web and Content Studio"/>
+        <meta property="twitter:description" content="Websites and content for creative entrepreneur"/>
   </Head>
 <Layout>
   <Component {...pageProps} />
