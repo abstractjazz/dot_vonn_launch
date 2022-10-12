bdf958137a51c722c4fa5e89dedf6deef381cb8b
abstractjazz <evan@heyevan.com>
2022-10-12 00:26:01 -0700

    GA key fix

--- pages/_app.js
+++ pages/_app.js
@@ -8,14 +8,14 @@ return (
   <>
   <Script
   strategy='afterInteractive' 
-  src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+  src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
   />
   <Script id="google-analytics" strategy='afterInteractive' >
     {`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', ${NEXT_PUBLIC_GOOGLE_ANALYTICS});
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
     `}
 </Script>
 
