5d79d9739e08060e1837d5dc174e8d553054b327
abstractjazz <evan@heyevan.com>
2022-10-11 21:44:44 -0700

    fixes typo

--- pages/_app.js
+++ pages/_app.js
@@ -6,15 +6,14 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
 return (
   <>
-  <Script id="loads-GA" strategy="lazyOnload" src={`<script async src="https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
+  <Script id="loads-GA" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}/>
   <Script id="GA-param" strategy="lazyOnload">
     {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',);
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}',);
     `}
-  
 </Script>
   <Head>
     <title>Dot Vonn</title>
